61f1b6199ada075a627bc4a039e3fcfd08efaede
Constantin Zamotaev <cossx@yahoo.com>
2021-03-11 16:24:09 +0400

    project added

---
+++ src/test/java/ru/netology/domain/PostmanTest.java
@@ -0,0 +1,24 @@
+package ru.netology.domain;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class PostmanTest {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()// Предусловия
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Aloha!")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", Matchers.equalTo("Aloha!"))
+        ;
+    }
+}
